a6457fe0556dd53c3a5d431d8840a920030b1b46
Sasha <sasha_vp@mail.ru>
2020-10-10 15:07:08 +0300

    первое задание выполнено

--- lesson7.py
+++ lesson7.py
@@ -13,10 +13,47 @@ while True:
               "Результатом сложения должна быть новая матрица."
               "Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки\n"
               "первой матрицы складываем с первым элементом первой строки второй матрицы и т.д.")
-        matrix_1 = {[1,2,3],
-                    [4,5,6],
-                    [7,8,9]
-        }
+        matrix = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]
+        matrix2 = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]
+
         class Matrix:
-            def __init__(self, matrix):
-                pass
+            def __init__(self, matrix_1):
+                self.matrix_1 = matrix_1
+
+            def __str__(self):
+                matr = str()
+
+                for el in self.matrix_1:
+                    ryad = str()
+
+                    for item in el:
+                        ryad += f"{item} "
+
+                    matr += f"{ryad}\n"
+
+                return matr
+
+            def __add__(self, other):
+                matrix_add = []
+                matrix_row = []
+                for i in range(len(self.matrix_1)):
+                    matrix_row.clear()
+                    for j in range(len(self.matrix_1[i])):
+                        matrix_row.append(self.matrix_1[i][j] + other.matrix_1[i][j])
+                    matrix_add.append(matrix_row[:])
+                return Matrix(matrix_add)
+
+        mat_1 = Matrix(matrix)
+        mat_2 = Matrix(matrix2)
+        print(mat_1 + mat_2)
+    elif
+    else:
+        print("Такого задания нет")
